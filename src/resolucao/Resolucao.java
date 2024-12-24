057dd2145e90c68464e106a0dced01792cffe785
Kleber Vales <ksvales@outlook.pt>
2024-12-24 09:29:25 -0300

    Resolucao do exercicio

--- src/resolucao/Resolucao.java
+++ src/resolucao/Resolucao.java
@@ -1,10 +1,67 @@
 package resolucao;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Flower {
+    String whatsYourName() {
+        return "I have many names and types.";
+    }
+}
+
+class Jasmine extends Flower {
+    @Override
+    String whatsYourName() {
+        return "Jasmine";
+    }
+}
+
+class Lily extends Flower {
+    @Override
+    String whatsYourName() {
+        return "Lily";
+    }
+}
+
+class Region {
+    Flower yourNationalFlower() {
+        return new Flower();
+    }
+}
+
+class WestBengal extends Region {
+    @Override
+    Jasmine yourNationalFlower() {
+        return new Jasmine();
+    }
+}
+
+class AndhraPradesh extends Region {
+    @Override
+    Lily yourNationalFlower() {
+        return new Lily();
+    }
+}
+
 public class Resolucao {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine().trim();
+        Region region = null;
+        switch (s) {
+            case "WestBengal":
+                region = new WestBengal();
+                break;
+            case "AndhraPradesh":
+                region = new AndhraPradesh();
+                break;
+        }
+        if (region != null) {
+            Flower flower = region.yourNationalFlower();
+            System.out.println(flower.whatsYourName());
+        } else {
+            System.out.println("Unknown region");
+        }
+    }
 }
